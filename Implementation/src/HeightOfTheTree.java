214d7313ed14c10c6bf9e550c1b2d89024f09a33
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-01-07 22:00:29 +0530

    File added

--- Implementation/src/HeightOfTheTree.java
+++ Implementation/src/HeightOfTheTree.java
@@ -1,36 +1,3 @@
-class Node {
-    public int key;
-    Node left;
-    Node right;
-    public Node(int k){
-        this.key=k;
-        left=right=null;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-}
 class HeightOfTheTree{
     public static void main(String[] args) {
         Node root = new Node(30);
