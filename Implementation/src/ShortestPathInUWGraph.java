a2fc1f487e616ac62200b3de66d8d15cd697c8c3
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-01-30 14:44:01 +0530

    Shirtest Path in an Unweighted Graph using BFS - Given an Unweighted Graph and a source point, the task is to find the shortest path between the source point and every other point in the graph.

---
+++ Implementation/src/ShortestPathInUWGraph.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestPathInUWGraph {
+    static void shortestpath(ArrayList<ArrayList<Integer>> adj, int v, int s, boolean[] visited){
+        int dist[]=new int[v];
+        dist[s]=0;
+        Queue<Integer> q=new LinkedList<>();
+        q.add(s);
+        visited[s]=true;
+        while (q.isEmpty()==false){
+            int u=q.poll();
+            for (int k: adj.get(u)){
+                if (visited[k]==false){
+                    dist[k]=dist[u]+1;
+                    visited[k]=true;
+                    q.add(k);
+                }
+            }
+        }
+        print(dist);
+    }
+
+    static void print(int[] dist) {
+        for (int i = 0; i < dist.length ; i++) {
+            System.out.print(dist[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        boolean visited[]=new boolean[V];
+        ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer>>(V);
+
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<Integer>());
+
+        Graph.addEdge(adj,0,1);
+        Graph.addEdge(adj,1,2);
+        Graph.addEdge(adj,2,3);
+        Graph.addEdge(adj,0,2);
+        Graph.addEdge(adj,1,3);
+        shortestpath(adj,V,0,visited);
+
+    }
+}
